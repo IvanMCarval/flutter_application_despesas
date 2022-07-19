5403abfa1398ab60b95d08c39fde7599e4b21a97
IVAN MASTRANGE <75397473+IvanMCarval@users.noreply.github.com>
2022-07-19 12:08:53 -0300

    Pequenas alterações

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -12,7 +12,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 300,
+      height: 400,
       child: transactions.isEmpty
           ? Column(
               children: [
@@ -50,7 +50,7 @@ class TransactionList extends StatelessWidget {
                         padding: const EdgeInsets.all(6),
                         child: FittedBox(
                           child: Text(
-                            'R\$${tr.value}',
+                            'R\$${tr.value.toStringAsFixed(2)}',
                             style: TextStyle(color: Colors.white),
                           ),
                         ),
